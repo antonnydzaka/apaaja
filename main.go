8a8054ad75a68826f9e831d8140da2b4aaa1d586
antonnydzaka <antonnydzaka@gmail.com>
2025-12-19 05:22:54 +0700

    apa

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 import "fmt"
-func main() {
-var bilangan, j int
-fmt.Scan(&bilangan)
-for j = 1; j <= bilangan; j += 1 {
-if bilangan % j == 0 {
-fmt.Print(j," ")
-}
-}
+func main(){
+	var warna1,warna2,warna3,warna4 string
+	var benar int
+	var hasil bool
+	for i := 1; i <= 5; i++ {
+		fmt.Printf("percobaan ke %v :",i)
+		fmt.Scan(&warna1,&warna2,&warna3,&warna4)
+		if warna1=="merah"&&warna2=="kuning"&&warna3=="hijau"&&warna4=="ungu"{
+			benar++
+		}
+	}
+	if benar == 5 {
+		hasil = true
+	}
+	fmt.Print(hasil)
 }
