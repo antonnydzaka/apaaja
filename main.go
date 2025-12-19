db2eafc9d903ac5dbb8314daf6c83174193a9172
antonnydzaka <antonnydzaka@gmail.com>
2025-12-20 04:17:50 +0700

    p

--- main.go
+++ main.go
@@ -1,18 +1,15 @@
 package main
 import "fmt"
 func main(){
-	var warna1,warna2,warna3,warna4 string
-	var benar int
-	var hasil bool
-	for i := 1; i <= 5; i++ {
-		fmt.Printf("percobaan ke %v :",i)
-		fmt.Scan(&warna1,&warna2,&warna3,&warna4)
-		if warna1=="merah"&&warna2=="kuning"&&warna3=="hijau"&&warna4=="ungu"{
-			benar++
+	var angka,total,banyak float32
+	for angka != 9999{
+		fmt.Scan(&angka)
+		total += angka 
+		banyak ++
+		if angka == 9999{
+			total -= 9999
+			banyak -= 1
 		}
 	}
-	if benar == 5 {
-		hasil = true
-	}
-	fmt.Print(hasil)
+	fmt.Print(total/banyak)
 }
