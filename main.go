e5ff74e978ac7e331af6ea2d40ac107dd4ea24fb
antonnydzaka <antonnydzaka@gmail.com>
2025-12-20 05:38:25 +0700

    p

--- main.go
+++ main.go
@@ -1,15 +1,29 @@
 package main
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 func main(){
-	var angka,total,banyak float32
-	for angka != 9999{
-		fmt.Scan(&angka)
-		total += angka 
-		banyak ++
-		if angka == 9999{
-			total -= 9999
-			banyak -= 1
+	var n int
+	var x, y float32
+	var A,B,C,D float32
+	fmt.Scan(&n)
+	for i := 0; i < n; i++ {
+		x = rand.Float32()
+		y = rand.Float32()
+		if x < 0.5 && y < 0.5 {
+			A++
+		} else if x >= 0.5 && y < 0.5 {
+			B++
+		} else if x >= 0.5 && y >= 0.5 {
+			C++
+		} else if x < 0.5 && y >= 0.5 {
+			D++
 		}
 	}
-	fmt.Print(total/banyak)
+	fmt.Printf("Daerah A: %f tetes (%.4f ml)\n", A, float64(A)*0.0001)
+	fmt.Printf("Daerah B: %f tetes (%.4f ml)\n", B, float64(B)*0.0001)
+	fmt.Printf("Daerah C: %f tetes (%.4f ml)\n", C, float64(C)*0.0001)
+	fmt.Printf("Daerah D: %f tetes (%.4f ml)\n", D, float64(D)*0.0001)
+
 }
