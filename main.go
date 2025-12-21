f5019006537575b0f31cfe921bea2cf66d72baa7
antonnydzaka <antonnydzaka@gmail.com>
2025-12-21 09:42:38 +0700

    arry

--- main.go
+++ main.go
@@ -1,23 +1,8 @@
 package main
-
-import (
-	"fmt"
-	"math/rand"
-)
-
+import "fmt"
 func main(){
-	var toping,topingpadapizza int
-	fmt.Scan(&toping)
-	var xc, yc, r float32 = 0.5, 0.5, 0.5
-	for i := 0; i < toping; i++ {
-		var x, y float32 = rand.Float32(),rand.Float32()
-		dx := x - xc
-		dy := y - yc
-		if (dx*dx + dy*dy) <= r*r{
-			topingpadapizza++
-		}
+	var apa = [...]string{"apel","jeruk","melon"}
+	for i, apakek := range apa{
+		fmt.Print(i,apakek)
 	}
-	var pi float32 = 4*float32(topingpadapizza)/float32(toping)
-	fmt.Println("toping pada pizza: ",topingpadapizza)
-	fmt.Println("phi :",pi)
 }
