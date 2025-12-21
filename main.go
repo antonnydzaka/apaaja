9f98166a21faee02ecd97ce6843e3e91c62cfab9
antonnydzaka <antonnydzaka@gmail.com>
2025-12-22 03:26:16 +0700

    salah ini belom di benerin

--- main.go
+++ main.go
@@ -2,14 +2,27 @@ package main
 
 import "fmt"
 
-func main() {
-	var chickens = []map[string]string{
-		{"name": "chicken blue", "gender": "male"},
-		{"name": "chicken red", "gender": "male"},
-		{"name": "chicken yellow", "gender": "female"},
+func main(){
+	var x,y int
+	fmt.Scan(&x,&y)
+	var fpb []int
+	var kpk []int
+	for i := 1; i <= x || i <= y; i++ {
+		if x%i==0 && y%i==0{
+			fpb = append(fpb, i)
+			x/=i
+			y/=i
+		}
 	}
-
-	for _, chicken := range chickens {
-		fmt.Println(chicken["gender"], chicken["name"])
+	for i := 1; i <= x || i <= y; i++ {
+		if x%i==0 || y%i==0{
+			kpk = append(kpk,i)
+			x/=i
+			y/=i
+		}
 	}
+	fmt.Println(fpb," ")
+	fmt.Println(kpk," ")
+	fmt.Println(len(fpb))
+	fmt.Println(cap(fpb))
 }
