caeecbeadb856e9ae687c8d36a85a034e06fe5d2
antonnydzaka <antonnydzaka@gmail.com>
2025-12-22 04:00:35 +0700

    belajar pak

--- main.go
+++ main.go
@@ -2,27 +2,22 @@ package main
 
 import "fmt"
 
-func main(){
-	var x,y int
-	fmt.Scan(&x,&y)
-	var fpb []int
-	var kpk []int
-	for i := 1; i <= x || i <= y; i++ {
-		if x%i==0 && y%i==0{
-			fpb = append(fpb, i)
-			x/=i
-			y/=i
+func main() {
+	var angka = []int{1, 5, 7, 9, 2}
+	var penjumlahan int
+	for i := 0; i < len(angka); i++ {
+		penjumlahan += angka[i]
+	}
+	fmt.Println(penjumlahan)
+	var average int = penjumlahan / len(angka)
+	fmt.Println(average)
+	var max, min int = angka[0], angka[0]
+	for i := range angka {
+		if max < angka[i] {
+			max = angka[i]
+		} else if min > angka[i] {
+			min = angka[i]
 		}
 	}
-	for i := 1; i <= x || i <= y; i++ {
-		if x%i==0 || y%i==0{
-			kpk = append(kpk,i)
-			x/=i
-			y/=i
-		}
-	}
-	fmt.Println(fpb," ")
-	fmt.Println(kpk," ")
-	fmt.Println(len(fpb))
-	fmt.Println(cap(fpb))
+	fmt.Println(max, min)
 }
