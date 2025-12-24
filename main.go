f615da6a578a24f9e95238e842083e00301e3343
antonnydzaka <antonnydzaka@gmail.com>
2025-12-25 04:23:12 +0700

    translate

--- main.go
+++ main.go
@@ -3,21 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	var angka = []int{1, 5, 7, 9, 2}
-	var penjumlahan int
-	for i := 0; i < len(angka); i++ {
-		penjumlahan += angka[i]
+	var input string
+	translate := map[string]string{
+		"home": "rumah",
+		"cat":  "kucing",
+		"cap":  "topi",
+		"hight":"tinggi",
+		"dog":"anjing",
 	}
-	fmt.Println(penjumlahan)
-	var average int = penjumlahan / len(angka)
-	fmt.Println(average)
-	var max, min int = angka[0], angka[0]
-	for i := range angka {
-		if max < angka[i] {
-			max = angka[i]
-		} else if min > angka[i] {
-			min = angka[i]
-		}
+	fmt.Scan(&input)
+	var value,ada = translate[input]
+	if ada{
+		fmt.Print(value)
+	}else{
+		fmt.Print("tidak ada didalam kamus")
 	}
-	fmt.Println(max, min)
+
 }
