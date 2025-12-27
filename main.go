a03f400ebbc5b455939c3219fcba4fe72caa928e
antonnydzaka <antonnydzaka@gmail.com>
2025-12-27 12:16:20 +0700

    heloo

--- main.go
+++ main.go
@@ -1,23 +1,5 @@
 package main
-
 import "fmt"
-
-func main() {
-	var input string
-	translate := map[string]string{
-		"home": "rumah",
-		"cat":  "kucing",
-		"cap":  "topi",
-		"hight":"tinggi",
-		"dog":"anjing",
-		"book":"buku",
-	}
-	fmt.Scan(&input)
-	var value,ada = translate[input]
-	if ada{
-		fmt.Print(value)
-	}else{
-		fmt.Print("tidak ada didalam kamus")
-	}
-
+func main(){
+	fmt.Print("helllo word")
 }
