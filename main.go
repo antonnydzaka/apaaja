a4c425e8dda6514cdc8b2b6b38065c4366bae9c7
antonnydzaka <antonnydzaka@gmail.com>
2026-01-08 21:29:32 +0700

    p

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
+
 import "fmt"
-func main(){
-	fmt.Print("helllo word")
+
+func main() {
+	var min, max int = 0, 100
+	var jawaban bool
+	var angka = 64
+	for max != min {
+		if angka < min {
+			fmt.Print(min)
+		} else {
+			fmt.Print(angka)
+		}
+		fmt.Scan(&jawaban)
+		if jawaban {
+			min += angka
+		} else {
+			max -= angka
+		}
+		angka /=2
+	}
+	fmt.Print("hasil : ", min)
 }
