67180963243562d7f04c57f2acc5ae5618236bc4
antonnydzaka <antonnydzaka@gmail.com>
2026-01-16 22:35:10 +0700

    p adu

--- latihan/main.go
+++ latihan/main.go
@@ -1,11 +1,14 @@
-package main
+package main 
 import "fmt"
-func main(){
-	var luas, keliling int = kotak(2,5)
-	fmt.Print(luas,keliling)
+func main (){
+	var avg = ratarata(1,2,3,4,5,6,7,4,3,2,2)
+	fmt.Print(avg)
 }
-func kotak(panjang int , lebar int )(int,int){
-	luas := panjang * lebar
-	keliling:= 2*(panjang + lebar)
-	return luas , keliling
+func ratarata(vels...int)float32{
+	var total int 
+	for _,vel:= range vels{
+		total += vel
+	}
+	var avg float32= float32(total)/float32(len(vels))
+	return avg
 }
